de434cba5e77cbfc41492076487666df22cfa0d6
qingbo.gao <qingbo.gao@nanguache.com>
2019-04-20 16:23:45 +0800

    init project

---
+++ src/main/java/com/qingbo/dailyutils/DailyutilsApplication.java
@@ -0,0 +1,13 @@
+package com.qingbo.dailyutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyutilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qingbo/dailyutils/DailyutilsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qingbo.dailyutils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DailyutilsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
